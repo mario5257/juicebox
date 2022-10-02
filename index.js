00afcd3b70f0f45eab2eed32420743ea7f055b5e
key <mario5257@att.net>
2022-10-01 22:30:52 -0700

    update

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ server.use(morgan('dev'));
 
 const client = new Client({
     connectionString: process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev',
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : undefined,
   });
 client.connect();
 server.use(express.json());
