ebedeb8586c945c1d8df796e248fa147db1b133f
key <mario5257@att.net>
2022-10-01 22:17:14 -0700

    corrections for database and server setup

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const PORT = 3000;
+const { PORT = 3000} = process.env;
 const express = require('express');
 const server = express();
 
 const morgan = require('morgan');
 server.use(morgan('dev'));
 
-const { client } = require('./db');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 client.connect();
 server.use(express.json());
 
